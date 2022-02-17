8ba3e0fc1b62732d820cc4f85ea4ef5d73af5ee3
Mahier Sydow <mahier.sydow8@gmail.com>
2022-02-17 15:31:01 +0900

    added in Modal as extra component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
 <template>
   <h1>{{ title }}</h1>
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
+  <Modal />
 </template>
 
 <script>
+import Modal from "./components/Modal.vue";
+
 export default {
   name: "App", // name is optional
+  components: {
+    Modal,
+  },
   data() {
     return {
       title: "My First Vue App",
     };
+  },
+  methods: {
+    handleClick() {
+      // accessing refs within the above input tag
+      console.log(this.$refs.name);
+      // to add a class, when we click on the button click me
+      this.$refs.name.classList.add("active");
+      // to focus on the input, when we click on the button click me
+      this.$refs.name.focus();
+    },
   },
 };
 </script>
